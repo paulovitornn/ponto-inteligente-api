7d7edce945ddc2efa2a21fc5158dfae9fc2c4227
Paulo Vitor <paulovitornn@gmail.com>
2023-01-28 17:13:09 -0300

    commit inicial do projeto

---
+++ src/test/java/com/pvn/pontointeligenteapi/PontoInteligenteApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pvn.pontointeligenteapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PontoInteligenteApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
